ce9770c67bfd621f659615e23de1eaf7aa887425
feroz khan <ferozkhan27@gmail.com>
2013-11-13 00:58:19 +0800

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -4,7 +4,7 @@ CELERYBEAT_SCHEDULE = {
     'add-every-10-seconds': {
         'task': 'web.start_harvesting',
         'schedule': timedelta(seconds=10),
-        'args': ('http://www.irwinwong.com/blog/how-the-fuji-x-series-made-me-feel-inadequate/', 'src="([^"]+)"')
+        'args': ('http://www.irwinwong.com/blog/how-the-fuji-x-series-made-me-feel-inadequate/', 'a')
     },
 }
 
