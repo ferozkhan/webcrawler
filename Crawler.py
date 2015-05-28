253a34fb06b607c96cf53788f6b2f9fb4229ae3d
ferozkhan <ferozkhan27@gmail.com>
2015-05-28 23:01:26 +0800

    update crawler, refector.

--- Crawler.py
+++ Crawler.py
@@ -1,4 +1,3 @@
-
 import logging
 import requests
 import InputData
@@ -6,44 +5,48 @@ import BeautifulSoup
 from multiprocessing import Pool
 from collections import namedtuple
 
-
-POOL_REQUIERD = 15
 INPUT = namedtuple('INPUT', ('url', 'scrap_formats'))
 logging.basicConfig(level=logging.INFO,
                     format='(%(threadName)-5s) %(processName)s %(message)s',
                     )
 
 
-def parse_input(raw_input):
-    return INPUT(raw_input.items()[0][0], raw_input.items()[0][1])
+def prepare_input(data):
+    """
+
+    :rtype : INPUT(url, scrap_formats)
+    """
+    return INPUT(data.items()[0][0], data.items()[0][1])
 
 
 def get_page_xml_source(url):
     return BeautifulSoup.BeautifulSoup(requests.get(url).text)
 
 
-def scrap_data(input):
-    xml_data = get_page_xml_source(input.url)
-    logging.info('scrapping: %s' % input.url)
-    for sf in input.scrap_formats:
+def scrap_data(data):
+    xml_data = get_page_xml_source(data.url)
+    logging.info('scrapping: %s' % data.url)
+    for sf in data.scrap_formats:
         try:
             element = sf.pop('element')
-            return ((input.url, element, sf),
-                    [d.getText(' ')
+            separator = sf.pop('separator') if 'separator' in sf else ''
+            return ((data.url, element, sf),
+                    [d.getText(separator)
                      for d in xml_data.findAll(element, attrs=sf)]
                     )
         except KeyError, e:
             logging.error(
-                "Key: '{}' must be provided. URL: {}".format(e, input.url)
+                "Key: '{}' must be provided. URL: {}".format(e, data.url)
             )
 
 
+def scrapy_do(scrap_formats_file_name, pool_required=5):
+    scrap_formats_file = InputData.JSONInputData(scrap_formats_file_name)
+    scraps_async_result = []
+    pool = Pool(pool_required)
+    for scrap_format in scrap_formats_file.read():
+        scraps_async_result.append(pool.apply_async(scrap_data, args=(prepare_input(scrap_format), )))
+
+
 if __name__ == '__main__':
-    pool = Pool(POOL_REQUIERD)
-    json_input = InputData.JSONInputData('input.json')
-    inputs = [parse_input(raw_input) for raw_input in json_input.read()]
-    results = [pool.apply_async(scrap_data, args=(input,)) for input in inputs]
-    pool.close()
-    pool.join()
-    for r in results:
-        logging.info(r.get())
+    scrapy_do('input.json')
